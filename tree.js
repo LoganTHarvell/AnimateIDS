fb6b7d9bc92d96fc78a1993947de04b8d4784984
LoganTHarvell <logan@harvell.us>
2018-11-22 00:01:23 -0500

    3rd times the charm indentation

--- tree.js
+++ tree.js
@@ -13,55 +13,55 @@ function Tree(_id) {
   this._root = new Node(_id);
 
   this.add_node = function(_id) {
-  if(this._root == null) {
-  this._root = new Node(_id);
-  }
-  else {
-  this.add_child(new Node(_id));
-  }
-  this.count++;
+    if(this._root == null) {
+      this._root = new Node(_id);
+    }
+    else {
+      this.add_child(new Node(_id));
+    }
+    this.count++;
   };
 
   this.add_child = function(_node) {
-  // If root doesn't have a child, give it one
-  if(this._root.children.length == 0) {
-  this._root.children.push(_node);
-  return;
-  };
+    // If root doesn't have a child, give it one
+    if(this._root.children.length == 0) {
+      this._root.children.push(_node);
+      return;
+    };
 
-  // The node to receive the miracle of birth
-  lucky_node = this._root;
+    // The node to receive the miracle of birth
+    lucky_node = this._root;
 
-  // Will we go down a level and make this lucky node a grand-node?
-  let go_down = (Math.random() >= 0.4);
+    // Will we go down a level and make this lucky node a grand-node?
+    let go_down = (Math.random() >= 0.4);
 
-  while(go_down) {
-  // If we go down a level and there are children,
-  // pick one to become the next lucky node
-  if(lucky_node.children.length > 0) {
-  index = get_random_index(lucky_node.children.length);
-  lucky_node = lucky_node.children[index];
-  // Check if we're going down again
-  go_down = (Math.random() >= 0.4);
-  }
-  // Otherwise, stick to this node
-  else {
-  go_down = false;
-  }
-  }
+    while(go_down) {
+      // If we go down a level and there are children,
+      // pick one to become the next lucky node
+      if(lucky_node.children.length > 0) {
+        index = get_random_index(lucky_node.children.length);
+        lucky_node = lucky_node.children[index];
+        // Check if we're going down again
+        go_down = (Math.random() >= 0.4);
+      }
+      // Otherwise, stick to this node
+      else {
+        go_down = false;
+      }
+    }
 
-  // Boom you have a child
-  lucky_node.children.push(_node);
+    // Boom you have a child
+    lucky_node.children.push(_node);
   };
 
   this.add = function(_id, to_id, traversal) {
     var child = new Node(_id),
-        parent = null,
-        callback = function(node) {
-        	if (node._id === to_id) {
-        		parent = node;
-        	}
-        };
+      parent = null,
+      callback = function(node) {
+      	if (node._id === to_id) {
+      		parent = node;
+      	}
+      };
 
     this.contains(callback, traversal);
 
@@ -75,35 +75,35 @@ function Tree(_id) {
 
   this.traverseDF = function(callback) {
 
-  (function recurse(node) {
-  for (var i = 0; i < node.children.length; i++) {
+    (function recurse(node) {
+      for (var i = 0; i < node.children.length; i++) {
       	recurse(node.children[i]);
-  }
+      }
 
     	callback(node);
-  })(this._root);
+    })(this._root);
 
   };
 
   this.traverseBF = function(callback) {
-  var queue = new Queue();
+    var queue = new Queue();
 
-  queue.enqueue(this._root);
+    queue.enqueue(this._root);
 
-  node = queue.dequeue();
-
-  while(node) {
-    for (var i = 0; i < node.children.length; i++) {
-      queue.enqueue(node.children[i]);
-    }
-
-    callback(node);
     node = queue.dequeue();
-  }
+
+    while(node) {
+      for (var i = 0; i < node.children.length; i++) {
+        queue.enqueue(node.children[i]);
+      }
+
+      callback(node);
+      node = queue.dequeue();
+    }
   };
 
   this.contains = function(callback, traversal) {
-  traversal.call(this, callback);
+    traversal.call(this, callback);
   };
 };
 
